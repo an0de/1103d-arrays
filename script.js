ed88c6ad8e11b4f48ccbc0c14172727fc4e067d0
an0de <149331026+an0de@users.noreply.github.com>
2023-11-10 11:08:48 +0200

    edit script.js

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
-}
+  const n = (arraySize > 0) ? arraySize : 0;
+  const a = [];
+  a.length = n;
+  return a.fill(value);
+};
 
-const data = 3;
+const size = 3;
 const valueToFill = 'a';
-console.log(fill(data, valueToFill))
+console.log(fill(size, valueToFill));
+
+console.log(fill());
+console.log(fill(-123, 1));
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  if (typeof (array) === 'object' && typeof (array.reverse) === 'function') {
+    return array.reverse();
+  }
+  if (typeof (array) === 'string') return [...array].reverse();
+  return [];
+};
 
-const data = [1, 2, 3];
+let data = [1, 2, 3];
 console.log(reverse(data)); // [3, 2, 1]
+console.log(reverse());
+console.log(reverse([]));
+console.log(reverse(123));
+console.log(reverse('asd'));
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
-}
+  const falsy = [0, false, undefined, '', null];
+  if (typeof (array) === 'object' && typeof (array.filter) === 'function') {
+    return array.filter((el) => !(falsy.includes(el) || Number.isNaN(el)));
+  }
+  return [];
+};
 
-const data = [0, 1, false, 2, undefined, '', 3, null];
-console.log(compact(data)) // [1, 2, 3]
+data = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data)); // [1, 2, 3]
+
+console.log(compact());
+console.log(compact([]));
+console.log(compact(123));
